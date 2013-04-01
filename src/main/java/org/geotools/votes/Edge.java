a47f776bd660898de972670d99f770dfe0c4c661
paynes <jan.koscak@gmail.com>
2013-04-01 22:47:30 +0200

    added edge implementation

--- src/main/java/org/geotools/votes/Edge.java
+++ src/main/java/org/geotools/votes/Edge.java
@@ -1,40 +1,47 @@
 package org.geotools.votes;
 
-public class Edge {
+public class Edge implements IEdge{
 
-	private Vertex v1;
-	private Vertex v2;
+    private IVertex v1;
+    private IVertex v2;
 	
-	public Edge(Vertex v1, Vertex v2) {
-		this.v1 = v1;
-		this.v2 = v2;
-	}
+    public Edge(Vertex v1, Vertex v2) {
+        this.v1 = v1;
+	this.v2 = v2;
+    }
 
-	public Vertex getV1() {
-		return this.v1;
-	}
+    public IVertex getV1() {
+	return this.v1;
+    }
 	
-	public Vertex getV2() {
-		return this.v2;
-	}
+    public IVertex getV2() {
+	return this.v2;
+    }
+    
+    @Override
+    public String toString() {
+	return "v1:" + this.v1.getId() + " v2:" + this.v2.getId();
+    }
 	
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj instanceof Edge) {
-			Edge e = (Edge) obj;
-			return (e.getV1().equals(getV1()) && e.getV2().equals(getV2())); 
-		}
-		return false;
-	}
+    @Override
+    public boolean equals(Object o) {
+	if (this == o) return true;
+        if (!(o instanceof Edge)) return false;
+        
+        Edge e = (Edge) o;
+        
+        if (!(this.v1.equals(e.getV1()))) return false;
+        if (!(this.v2.equals(e.getV2()))) return false;
+        
+	return true;
+    }
 	
+    @Override
     public int hashCode() {
     	int hash = 17;
     	hash = 37 * hash + v1.hashCode();
     	hash = 37 * hash + v2.hashCode();
     	return hash;
-        }
-	
-	public String toString() {
-		return v1.getId() + " " + v2.getId();
-	}	
+    }
+		
 }
