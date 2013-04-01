8d0507c40943db744442ee18bd94f9b32ddc8cf7
paynes <jan.koscak@gmail.com>
2013-04-01 22:25:49 +0200

    added javaDoc

--- src/main/java/org/geotools/votes/IVertex.java
+++ src/main/java/org/geotools/votes/IVertex.java
@@ -13,33 +13,94 @@ import java.util.ArrayList;
 
 public interface IVertex {
     
+    /**
+     * 
+     * @return Vertex id
+     */
     public Integer getId();
     
+    /**
+     * 
+     * @param vertex add neighbour vertex
+     */
     public void addNeighbourVertex(Vertex vertex);
     
+    /**
+     * 
+     * @param vertex remove neighbour vertex
+     */
     public void removeNeighbourVertex(Vertex vertex);
     
+    /**
+     * 
+     * @return neighbour vertices
+     */
     public ArrayList<IVertex> getNeighbourVertices();
     
+    /**
+     * 
+     * @return number of democrats electors
+     */
     public Integer getDemRes();
     
+    /**
+     * 
+     * @param res set number of democrats electors
+     */
     public void setDemRes(Integer res);
     
+    /**
+     * 
+     * @return number of republicans electors
+     */
     public Integer getRepRes();
     
+    /**
+     * 
+     * @param res set number of republicans electors
+     */
     public void setRepRes(Integer res);
     
+    /**
+     * 
+     * @param cov set number of region
+     */
     public void setCover(Integer cov);
     
+    /**
+     * 
+     * @return number of region
+     */
     public Integer getCover();
     
+    /**
+     * 
+     * @return total electors
+     */
     public Integer getScale();
     
+    /**
+     * 
+     * @return difference the number of democrats and number of republicans (dem - rep)
+     */
     public Integer getDemEvaluation();
     
+    /**
+     * 
+     * @return difference the number of republicans and democrats (rep - dem)
+     */
     public Integer getRepEvaluation();
     
+    /**
+     * Indicates whether we can remove vertex from graph and graph still remains connected
+     * @return true or false
+     */
     public boolean getCut();
     
+    /**
+     * Indicates whether we can remove vertex from graph and graph still remains connected
+     * 
+     * @param cut set true or false
+     */
     public void setCut(boolean cut);
 }
